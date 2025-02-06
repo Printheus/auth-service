348b0a85f676d78291a556005599cebfc94bd10b
amin masoudi <aminmasoudi2003@gmail.com>
2025-02-07 00:08:24 +0330

    add config manager

---
+++ auth_service/core/config_manager.py
@@ -0,0 +1,18 @@
+import importlib
+
+
+class ConfigManager:
+    def __init__(self) -> None:
+        mod = importlib.import_module("core.config")
+        for setting in dir(mod):
+            if setting.isupper():
+                print(setting)
+                setting_value = getattr(mod, setting)
+                setattr(self, setting, setting_value)
+
+
+    @property
+    def db_conf(self):
+        return self.DATABASE
+
+conf = ConfigManager()
