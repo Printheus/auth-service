7db190079585f40dc570319823817b02b80d05f1
amin masoudi <aminmasoudi2003@gmail.com>
2025-02-20 17:26:32 +0330

    add sign in with react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignInSide from './temp/SignInSide/SignInSide';
+// import SignInSide from './SignInSide';
 
-function App() {
 
+
+const App: React.FC = () => {
   return (
-    <>
-    </>
-  )
-}
+      <Router>
+        <Routes>
+          <Route path='/' element={<SignInSide/>}/>
+        </Routes>
+      </Router>
+  );
+};
 
-export default App
+export default App;
