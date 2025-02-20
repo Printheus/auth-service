c362bf25f13579fa13448ceed67fb4a2b3e72026
amin masoudi <aminmasoudi2003@gmail.com>
2025-02-20 13:38:20 +0330

    remove user create schema

--- auth_service/core/controllers.py
+++ auth_service/core/controllers.py
@@ -6,7 +6,6 @@ import sqlalchemy as sa
 
 from .models import User, uuid
 from .utils import bcrypt_hasher
-from . import schema
 
 
 class UserController:
@@ -18,9 +17,9 @@ class UserController:
         result = await self.db.scalars(query)
         return result.first()
 
-    async def create(self, user: schema.UserCreate):
-        user.password = bcrypt_hasher(user.password).decode()
-        user = User(**user.model_dump())
+    async def create(self, user: dict[str:str]) -> User:
+        user["password"] = bcrypt_hasher(user["password"]).decode()
+        user = User(**user)
         self.db.add(user)
         try:
             await self.db.commit()
@@ -32,8 +31,6 @@ class UserController:
         return user
 
     async def retrieve_by_username(self, username: str) -> User | None:
-        query = sa.select(User).where(
-            User.username == username
-        )
+        query = sa.select(User).where(User.username == username)
         result = await self.db.scalars(query)
         return result.first()
