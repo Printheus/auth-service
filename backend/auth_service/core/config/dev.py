96831a1bbba26519766c4f0943159f78e79e1c3c
amin masoudi <aminmasoudi2003@gmail.com>
2025-03-14 15:31:11 +0330

    change to mysql

--- backend/auth_service/core/config/dev.py
+++ backend/auth_service/core/config/dev.py
@@ -1,5 +1,9 @@
 DATABASE = {
-    "drivername": "sqlite+aiosqlite",
-    "database": "db.sqlite3",
+    "drivername": "mysql+aiomysql",
+    "database": "mydb",
+    "username": "amin",
+    "password": "amin2323",
+    "host": "127.0.0.1",
+    "port": 3306
 }
 PORT = 8000
