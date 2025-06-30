d48d9738cc11facc81806612cb80331b857c1e7c
amin masoudi <aminmasoudi2003@gmail.com>
2025-06-30 23:33:43 +0330

    Fuck sqlalchemy

--- backend-new/app/models.py
+++ backend-new/app/models.py
@@ -7,21 +7,22 @@ from .database import Base
 class User(Base):
     __tablename__ = "Users"
 
-    password: Mapped[str]
-    username: Mapped[str] = mapped_column(unique=True)
-    phone: Mapped[str | None] = mapped_column(nullable=True, unique=True, default=None)
-    email: Mapped[str | None] = mapped_column(nullable=True, unique=True, default=None)
+    password: Mapped[str] = mapped_column(sa.String(255)) 
+    username: Mapped[str] = mapped_column(sa.String(255), unique=True)
+    phone: Mapped[str | None] = mapped_column(sa.String(255), nullable=True, unique=True, default=None)
+    email: Mapped[str | None] = mapped_column(sa.String(255), nullable=True, unique=True, default=None)
     is_active: Mapped[bool] = mapped_column(default=True)
-    first_name: Mapped[str|None] = mapped_column(default="")
-    last_name: Mapped[str|None] = mapped_column(default="")
-    is_staff: Mapped[bool] = mapped_column(default=False)
-    date_joined: Mapped[datetime.datetime] = mapped_column(sa.DateTime, default=None)
-    is_superuser: Mapped[bool] = mapped_column(default=False)
+    first_name: Mapped[str|None] = mapped_column(sa.String(255), default="")
+    last_name: Mapped[str|None] = mapped_column(sa.String(255), default="")
+    is_staff: Mapped[bool] = mapped_column(sa.String(255), default=False)
+    date_joined: Mapped[datetime.datetime] = mapped_column( sa.DateTime, default=None)
+    is_superuser: Mapped[bool] = mapped_column(sa.String(255), default=False)
     user_id: Mapped[str] = mapped_column(
-        sa.types.UUID,
+        sa.Uuid,
         primary_key=True,
         unique=True,
         nullable=False,
         default_factory=uuid.uuid4,
         index=True,
     )
+
